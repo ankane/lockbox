220a132948ffa9efa438012bdb23c788ea68fc8f
Andrew Kane <andrew@chartkick.com>
2019-01-02 11:29:39 -0800

    Skip XChaCha20 tests on Travis

--- test/lockbox_test.rb
+++ test/lockbox_test.rb
@@ -39,6 +39,8 @@ class LockboxTest < Minitest::Test
   end
 
   def test_xchacha20
+    skip if travis?
+
     box = Lockbox.new(key: random_key, algorithm: "xchacha20")
     message = "it works!"
     ciphertext = box.encrypt(message)
@@ -50,6 +52,8 @@ class LockboxTest < Minitest::Test
   end
 
   def test_xchacha20_associated_data
+    skip if travis?
+
     box = Lockbox.new(key: random_key, algorithm: "xchacha20")
     message = "it works!"
     associated_data = "boom"
@@ -85,6 +89,8 @@ class LockboxTest < Minitest::Test
   end
 
   def test_bad_ciphertext_xchacha20
+    skip if travis?
+
     box = Lockbox.new(key: random_key, algorithm: "xchacha20")
 
     assert_raises(Lockbox::DecryptionError) do
@@ -116,6 +122,8 @@ class LockboxTest < Minitest::Test
   end
 
   def test_xchacha20_inspect
+    skip if travis?
+
     box = Lockbox.new(key: random_key, algorithm: "xchacha20")
     refute_includes box.inspect, "key"
     refute_includes box.to_s, "key"
@@ -137,6 +145,8 @@ class LockboxTest < Minitest::Test
   end
 
   def test_xchacha20_hex_key
+    skip if travis?
+
     box = Lockbox.new(key: SecureRandom.hex(32), algorithm: "xchacha20")
     message = "it works!"
     ciphertext = box.encrypt(message)
@@ -147,5 +157,9 @@ class LockboxTest < Minitest::Test
 
   def random_key
     SecureRandom.random_bytes(32)
+  end
+
+  def travis?
+    ENV["TRAVIS"]
   end
 end
