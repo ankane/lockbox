8ea49bf580b37bbf8b4f83204ed75561391af937
Andrew Kane <andrew@chartkick.com>
2019-12-14 14:43:07 -0800

    Moved migration code to separate class

--- lib/lockbox.rb
+++ lib/lockbox.rb
@@ -7,6 +7,7 @@ require "lockbox/box"
 require "lockbox/encryptor"
 require "lockbox/key_generator"
 require "lockbox/io"
+require "lockbox/migrator"
 require "lockbox/model"
 require "lockbox/utils"
 require "lockbox/version"
@@ -41,51 +42,7 @@ class Lockbox
   end
 
   def self.migrate(model, restart: false)
-    # get fields
-    fields = model.lockbox_attributes.select { |k, v| v[:migrating] }
-
-    # get blind indexes
-    blind_indexes = model.respond_to?(:blind_indexes) ? model.blind_indexes.select { |k, v| v[:migrating] } : {}
-
-    # build relation
-    relation = model.unscoped
-
-    unless restart
-      attributes = fields.map { |_, v| v[:encrypted_attribute] }
-      attributes += blind_indexes.map { |_, v| v[:bidx_attribute] }
-
-      if defined?(ActiveRecord::Base) && model.is_a?(ActiveRecord::Base)
-        attributes.each_with_index do |attribute, i|
-          relation =
-            if i == 0
-              relation.where(attribute => nil)
-            else
-              relation.or(model.unscoped.where(attribute => nil))
-            end
-        end
-      end
-    end
-
-    if relation.respond_to?(:find_each)
-      relation.find_each do |record|
-        migrate_record(record, fields: fields, blind_indexes: blind_indexes, restart: restart)
-      end
-    else
-      relation.all.each do |record|
-        migrate_record(record, fields: fields, blind_indexes: blind_indexes, restart: restart)
-      end
-    end
-  end
-
-  # private
-  def self.migrate_record(record, fields:, blind_indexes:, restart:)
-    fields.each do |k, v|
-      record.send("#{v[:attribute]}=", record.send(k)) if restart || !record.send(v[:encrypted_attribute])
-    end
-    blind_indexes.each do |k, v|
-      record.send("compute_#{k}_bidx") if restart || !record.send(v[:bidx_attribute])
-    end
-    record.save(validate: false) if record.changed?
+    Migrator.new(model).migrate(restart: restart)
   end
 
   def initialize(**options)
---
+++ lib/lockbox/migrator.rb
@@ -0,0 +1,58 @@
+class Lockbox
+  class Migrator
+    def initialize(model)
+      @model = model
+    end
+
+    def migrate(restart:)
+      model = @model
+
+      # get fields
+      fields = model.lockbox_attributes.select { |k, v| v[:migrating] }
+
+      # get blind indexes
+      blind_indexes = model.respond_to?(:blind_indexes) ? model.blind_indexes.select { |k, v| v[:migrating] } : {}
+
+      # build relation
+      relation = model.unscoped
+
+      unless restart
+        attributes = fields.map { |_, v| v[:encrypted_attribute] }
+        attributes += blind_indexes.map { |_, v| v[:bidx_attribute] }
+
+        if defined?(ActiveRecord::Base) && model.is_a?(ActiveRecord::Base)
+          attributes.each_with_index do |attribute, i|
+            relation =
+              if i == 0
+                relation.where(attribute => nil)
+              else
+                relation.or(model.unscoped.where(attribute => nil))
+              end
+          end
+        end
+      end
+
+      if relation.respond_to?(:find_each)
+        relation.find_each do |record|
+          migrate_record(record, fields: fields, blind_indexes: blind_indexes, restart: restart)
+        end
+      else
+        relation.all.each do |record|
+          migrate_record(record, fields: fields, blind_indexes: blind_indexes, restart: restart)
+        end
+      end
+    end
+
+    private
+
+    def migrate_record(record, fields:, blind_indexes:, restart:)
+      fields.each do |k, v|
+        record.send("#{v[:attribute]}=", record.send(k)) if restart || !record.send(v[:encrypted_attribute])
+      end
+      blind_indexes.each do |k, v|
+        record.send("compute_#{k}_bidx") if restart || !record.send(v[:bidx_attribute])
+      end
+      record.save(validate: false) if record.changed?
+    end
+  end
+end
