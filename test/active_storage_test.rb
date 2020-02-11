938607766bd9707d1562a576ef704bc8e51fa4ff
Andrew Kane <andrew@chartkick.com>
2020-02-10 17:18:21 -0800

    Better tests [skip ci]

--- test/active_storage_test.rb
+++ test/active_storage_test.rb
@@ -2,7 +2,7 @@ require_relative "test_helper"
 
 class ActiveStorageTest < Minitest::Test
   def setup
-    skip unless defined?(ActiveStorage) && !mongoid?
+    skip unless defined?(ActiveStorage)
   end
 
   def test_encrypt_one
