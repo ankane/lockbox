1b4c7d61b0dd8952301f76c70fdef2cb79780250
Andrew Kane <andrew@chartkick.com>
2020-04-13 15:01:56 -0700

    Added comment [skip ci]

--- lib/lockbox/model.rb
+++ lib/lockbox/model.rb
@@ -227,6 +227,7 @@ module Lockbox
             message = super()
 
             # Hash serializer returns {} when nil, Array serializer returns [] when nil
+            # possibly keep track of decrypted attributes directly in the future
             if message.nil? || (activerecord && @attributes[name.to_s].value_before_type_cast.nil?)
               ciphertext = send(encrypted_attribute)
               message = self.class.send(decrypt_method_name, ciphertext, context: self)
