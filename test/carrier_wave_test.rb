e3c22dcd922cd58fceb9122fb9ac28ff5f3c89ee
Andrew Kane <andrew@chartkick.com>
2020-05-11 17:31:21 -0700

    Better image tests

--- test/carrier_wave_test.rb
+++ test/carrier_wave_test.rb
@@ -48,54 +48,47 @@ class CarrierWaveTest < Minitest::Test
   end
 
   def test_image
-    path = "test/support/image.png"
     uploader = AvatarUploader.new
-    uploader.store!(File.open(path))
+    uploader.store!(image_file)
 
     assert_equal "image/png", uploader.content_type
-    assert_equal File.binread(path), uploader.read
+    assert_equal image_content, uploader.read
 
     uploader = AvatarUploader.new
     uploader.retrieve_from_store!("image.png")
 
     assert_equal "image/png", uploader.content_type
-    assert_equal File.binread(path), uploader.read
+    assert_equal image_content, uploader.read
   end
 
   def test_mounted
     skip if mongoid?
 
-    path = "test/support/image.png"
-    message = File.binread(path)
-    file = File.open(path)
+    user = User.create!(document: image_file)
 
-    user = User.create!(document: file)
-
-    assert_equal message, user.document.read
-    refute_equal message, user.document.file.read
+    assert_equal image_content, user.document.read
+    assert_equal "image/png", user.document.content_type
+    refute_equal image_content, user.document.file.read
 
     user = User.last
-    assert_equal message, user.document.read
+    assert_equal image_content, user.document.read
     assert_equal "image/png", user.document.content_type
-    refute_equal message, user.document.file.read
+    refute_equal image_content, user.document.file.read
   end
 
   def test_mounted_many
     skip if mongoid?
 
-    path = "test/support/image.png"
-    message = File.binread(path)
-    file = File.open(path)
+    user = User.create!(documents: [image_file])
 
-    user = User.create!(documents: [file])
-
-    assert_equal message, user.documents.first.read
-    refute_equal message, user.documents.first.file.read
+    assert_equal image_content, user.documents.first.read
+    assert_equal "image/png", user.documents.first.content_type
+    refute_equal image_content, user.documents.first.file.read
 
     user = User.last
-    assert_equal message, user.documents.first.read
+    assert_equal image_content, user.documents.first.read
     assert_equal "image/png", user.documents.first.content_type
-    refute_equal message, user.documents.first.file.read
+    refute_equal image_content, user.documents.first.file.read
   end
 
   def content
@@ -107,5 +100,13 @@ class CarrierWaveTest < Minitest::Test
     file.write(content)
     file.rewind
     file
+  end
+
+  def image_content
+    File.binread("test/support/image.png")
+  end
+
+  def image_file
+    File.open("test/support/image.png", "rb")
   end
 end
