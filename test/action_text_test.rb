37102d38eec64798cafa277bfd7a43bb3ef11189
Andrew Kane <andrew@chartkick.com>
2020-05-11 17:37:58 -0700

    Added additional Active Text test

--- test/action_text_test.rb
+++ test/action_text_test.rb
@@ -5,8 +5,16 @@ class ActionTextTest < Minitest::Test
     skip unless defined?(ActionText)
   end
 
-  def test_works
+  def test_create
     user = User.create!(content: "hi")
     assert_equal "<div class=\"trix-content\">\n  hi\n</div>\n", user.content.body.to_s
+  end
+
+  # if encrypted, the ciphertext will change when the content is the same
+  def test_encrypted
+    user = User.create!(content: "hi")
+    original_ciphertext = user.content.body_ciphertext
+    user.update!(content: "hi")
+    refute_equal user.content.body_ciphertext, original_ciphertext
   end
 end
