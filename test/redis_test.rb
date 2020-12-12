a47070e855d6ece3bd246ac07a586311dbac7d02
Andrew Kane <andrew@ankane.org>
2020-12-12 14:26:37 -0800

    Created helper method [skip ci]

--- test/redis_test.rb
+++ test/redis_test.rb
@@ -6,20 +6,22 @@ class RedisTest < Minitest::Test
   end
 
   def test_works
-    encrypted_redis = Lockbox::Redis.new(key: random_key, logger: $logger)
     encrypted_redis.set("hello", "world")
     assert_equal "world", encrypted_redis.get("hello")
     refute_equal "world", redis.get("hello")
   end
 
   def test_missing
-    encrypted_redis = Lockbox::Redis.new(key: random_key, logger: $logger)
     assert_nil encrypted_redis.get("hello")
     assert_nil redis.get("hello")
   end
 
   def redis
     @redis ||= Redis.new(logger: $logger)
+  end
+
+  def encrypted_redis
+    @encrypted_redis ||= Lockbox::Redis.new(key: random_key, logger: $logger)
   end
 
   def random_key
