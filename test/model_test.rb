5ed863233057e24f0c3f0eac936626b67c45938a
Andrew Kane <andrew@ankane.org>
2022-06-11 17:44:45 -0700

    Updated todo [skip ci]

--- test/model_test.rb
+++ test/model_test.rb
@@ -227,7 +227,7 @@ class ModelTest < Minitest::Test
     assert !user.has_attribute?("name")
     assert !user.has_attribute?(:name)
 
-    # TODO try to make virtual attribute behavior consistent in 0.8.0
+    # TODO try to make virtual attribute behavior consistent
     # this may be difficult, as virtual attributes are set to self.class._default_attributes
     # which gets merged with query attributes in initialize method of active_record/core.rb
     # assert_equal ["id"], user.attributes.keys
