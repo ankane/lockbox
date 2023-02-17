8baefd562495439af3292fba329d5861105678be
JcDores <j.dores@runtime-revolution.com>
2023-02-17 18:10:49 +0000

    Update Calculations

--- lib/lockbox/calculations.rb
+++ lib/lockbox/calculations.rb
@@ -12,16 +12,16 @@ module Lockbox
 
       return super unless lockbox_columns.any?
 
-      associated_columns_names = column_names.dup
+      associated_columns_names = []
       # replace column with ciphertext column
       lockbox_columns.each do |la, i|
         column_names[i] = la[:encrypted_attribute]
-        associated_columns_names[i] = la[:with_associated_field] if la[:with_associated_field]
+        associated_columns_names.push(la[:with_associated_field]) if la[:with_associated_field]
       end
 
       # pluck
-      all_columns = (column_names + associated_columns_names).compact.uniq
-      result = super(*column_names + associated_columns_names)
+      all_columns = (column_names + associated_columns_names).map(&:to_s).compact.uniq
+      result = super(*all_columns)
       result_hash = result.map { |fields| all_columns.zip(fields).to_h }
 
       # decrypt result
@@ -29,14 +29,14 @@ module Lockbox
       #
       # we can't pass context to decrypt method
       # so this won't work if any options are a symbol or proc
-
       lockbox_columns.each do |la, i|
         encrypted_attr = la[:encrypted_attribute]
-        associated_attr = la[:with_associated_field]
+        associated_attr = la[:with_associated_field] || nil
         result_hash.each do |record|
           record[encrypted_attr] = model.send("decrypt_#{la[:encrypted_attribute]}", record[encrypted_attr], record[associated_attr].to_s || '')
         end
       end
+
       result = result_hash.map { |record| record.slice(*column_names).values }
       result.flatten! if column_names.size == 1
       result
