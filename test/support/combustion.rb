f9cd57830f416f2716f5bfd49bd9f128469e3834
Andrew Kane <andrew@ankane.org>
2024-10-26 13:59:22 -0700

    Removed unneeded test code

--- test/support/combustion.rb
+++ test/support/combustion.rb
@@ -7,10 +7,6 @@ Lockbox.encrypts_action_text_body
 Combustion.initialize!(*components) do
   config.load_defaults Rails.version.to_f
 
-  if ActiveRecord::VERSION::STRING.to_f == 7.0
-    config.active_record.legacy_connection_handling = false
-  end
-
   config.logger = $logger
 
   config.time_zone = "Mountain Time (US & Canada)"
@@ -18,10 +14,6 @@ Combustion.initialize!(*components) do
   config.active_job.queue_adapter = :inline
 
   config.active_storage.service = :test
-
-  if ActiveRecord::VERSION::STRING.to_f == 7.0
-    config.active_storage.replace_on_assign_to_many = true
-  end
 
   # TODO remove
   config.active_record.yaml_column_permitted_classes = [Symbol, Time]
